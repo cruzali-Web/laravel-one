3c439dd91ec23bf7ff705e6f2dab41eb79b8e496
Ali Cruz <ali.cruz@me.com>
2024-02-26 07:44:51 -0600

    update

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Your Laravel Project</title>
+
+    <!-- Include the compiled CSS file -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <!-- Your HTML content here -->
+    <div class="back">
+      hola
+
+    </div>
+
+    <!-- Scripts -->
+   
+</body>
+</html>
